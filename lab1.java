b248a1614bc3231f246563d6285a671b5a24dac6
bcreech40 <54713424+bcreech40@users.noreply.github.com>
2019-08-30 15:32:55 -0400

    E1.8

---
+++ lab1.java
@@ -0,0 +1,9 @@
+public class lab1 {
+  public static void main(String[] args){
+    System.out.println("+~~~~~~+");
+    System.out.println("| D  D |");
+    System.out.println("|  V   |");
+    System.out.println("| ---  |");
+    System.out.println("+------+");
+  }
+}
